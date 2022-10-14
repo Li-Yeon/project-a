2110aad3dbc457fa435a5da1a3fc8cfde3eeab58
Li-Yeon <mekaleon27@gmail.com>
2022-10-14 17:02:29 +0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+        Oswald: ['"Oswald"', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
