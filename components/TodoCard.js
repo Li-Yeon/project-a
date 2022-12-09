4f3c660282355f86629b0a0a0b23c05880f3b53b
Li Yeon <mekaleon27@gmail.com>
2022-12-10 03:31:11 +0800

    Pull notes from backend
    + Using axios to fetch data from backend
    + Render ToDoCard with data from backend

--- components/TodoCard.js
+++ components/TodoCard.js
@@ -1,14 +1,30 @@
 import React from 'react'
 
 function TodoCard(props) {
+
+    let readableDate = new Date(props.date);
+
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        timeZone: 'GMT'
+    }
+
+    const timeOptions = {
+        hour: '2-digit',
+        minute: '2-digit'
+    }
+
     return (
         <div className="min-h-[100px] md:min-w-[15%] md:max-w-[270px] md:min-h-[150px] 
         shadow-sm md:hover:shadow-lg p-4 rounded-md md:hover:scale-105 cursor-pointer w-full bg-[#6B728E] text-white">
 
             <h1 className='font-Montserrat font-bold text-ellipsis md:max-w-[270px] whitespace-nowrap overflow-hidden'>{props.title}</h1>
-            <h1 className='font-Montserrat text-sm'>[{props.owner}]</h1>
-            <h1 className='font-Montserrat mt-4 text-sm'>{props.desc}</h1>
-            <h1 className='font-Montserrat mt-4 text-xs font-bold'>{props.date}</h1>
+            <h1 className='font-Montserrat text-sm'>[{props.name}]</h1>
+            <h1 className='font-Montserrat mt-4 text-sm'>{props.description}</h1>
+            <h1 className='font-Montserrat mt-4 text-xs font-bold'>{readableDate.toLocaleTimeString([], timeOptions)} - 
+            {readableDate.toLocaleDateString('en-MY', options)}</h1>
 
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,45 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
+
+// Utilities
+import axios from 'axios'
 
 // Components
 import TodoCard from '../components/TodoCard'
 
 export default function Home() {
+
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    // Call the GetAllNotes function to fetch the notes
+    GetAllNotes();
+  }, []);
+
+  async function GetAllNotes() {
+    try {
+      const response = await axios.get(process.env.NEXT_PUBLIC_API_URL + 'note/getallnote');
+      const result = response.data.data;
+      setNotes(result);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <Head>
         <title>To-Do App</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="#"></link>
       </Head>
       <div className='flex gap-4 flex-wrap'>
-        <TodoCard owner={"Leon"} title={"Project-A"} desc={"Make a To-Do App using NextJS and NodeJS (MongoDB)"} date={"8.05PM - 9/12/2022"} />
+        {
+          notes.map((item) => {
+            return <TodoCard key={item._id} title={item.title} name={item.name} description={item.description} date={item.createdAt} />
+          })
+        }
       </div>
     </>
   )
