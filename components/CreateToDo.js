f14c411ac663e158abf909a8d11d98e45a287dba
Li Yeon <mekaleon27@gmail.com>
2022-12-10 15:26:46 +0800

    Implemented create note
    + Implemented create note to MongoDB

--- components/CreateToDo.js
+++ components/CreateToDo.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 
 function CreateToDo(props) {
+
+  const [title, setTitle] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [error, setError] = useState(false)
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const newNote = { title, name, description };
+    const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + 'note/createnote', newNote);
+
+    console.log(response)
+
+    if (response.data.status == 201) {
+      setTitle('');
+      setName('');
+      setDescription('');
+      setError(false);
+    }
+    else {
+      setError(true);
+    }
+
+  }
+
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
 
-      <div className='w-[90%] md:max-w-[500px] h-[300px] bg-[#50577A] border border-[#404258] shadow-md rounded-md relative'>
+      <div className='w-[90%] md:max-w-[500px] h-[310px] bg-[#50577A] border border-[#404258] shadow-md rounded-md relative'>
 
         <div className="absolute top-3 right-8">
           <button className='bg-[#6B728E] p-1 rounded-md hover:bg-[#474E68]'
@@ -14,28 +42,42 @@ function CreateToDo(props) {
         </div>
 
         <div className='flex flex-col py-6 px-8'>
+          <form action="" onSubmit={handleSubmit}>
+            {/* Title */}
+            <div className='flex flex-col gap-1 mt-4'>
+              <h1 className='font-Montserrat text-md text-white'>Title</h1>
+              <input type="text" className='w-full rounded-sm px-2 font-Montserrat text-md'
+                required
+                onChange={(e) => setTitle(e.target.value)}
+                value={title} />
+            </div>
 
-          {/* Title */}
-          <div className='flex flex-col gap-1 mt-4'>
-            <h1 className='font-Montserrat text-md text-white'>Title</h1>
-            <input required type="text" className='w-full rounded-sm px-2 font-Montserrat text-md' />
-          </div>
+            {/* Name */}
+            <div className='flex flex-col gap-1 mt-2'>
+              <h1 className='font-Montserrat text-md text-white'>Name</h1>
+              <input type="text" className='w-full rounded-sm px-2 font-Montserrat text-md'
+                onChange={(e) => setName(e.target.value)}
+                value={name} />
+            </div>
 
-          {/* Title */}
-          <div className='flex flex-col gap-1 mt-2'>
-            <h1 className='font-Montserrat text-md text-white'>Name</h1>
-            <input required type="text" className='w-full rounded-sm px-2 font-Montserrat text-md' />
-          </div>
+            {/* Description */}
+            <div className='flex flex-col gap-1 mt-2'>
+              <h1 className='font-Montserrat text-md text-white'>Description</h1>
+              <textarea type="text" className='w-full rounded-sm px-2 font-Montserrat text-md resize-none'
+                onChange={(e) => setDescription(e.target.value)}
+                value={description} />
+            </div>
 
-          {/* Description */}
-          <div className='flex flex-col gap-1 mt-2'>
-            <h1 className='font-Montserrat text-md text-white'>Description</h1>
-            <textarea type="text" className='w-full rounded-sm px-2 font-Montserrat text-md resize-none' />
-          </div>
+            <button className='bg-[#6B728E] p-1 mt-4 rounded-md hover:bg-[#474E68]'>
+              <h1 className='font-Montserrat text-sm px-1 text-white'>Create Note</h1>
+            </button>
 
-          <button className='bg-[#6B728E] p-1 mt-4 rounded-md hover:bg-[#474E68]'>
-            <h1 className='font-Montserrat text-sm px-1 text-white'>Create Note</h1>
-          </button>
+            {/* Error Handling */}
+            {error && (
+              <h1 className='font-Montserrat text-xs text-red-500 italic mt-1'>* An error has occured.</h1>
+            )}
+
+          </form>
 
         </div>
 
--- components/TodoCard.js
+++ components/TodoCard.js
@@ -3,6 +3,14 @@ import React from 'react'
 function TodoCard(props) {
 
     let readableDate = new Date(props.date);
+    let modifiedName
+
+    if(props.name != '') {
+        modifiedName = '[' + props.name + ']';
+    } else
+    {
+        modifiedName = '[N/A]'
+    }
 
     const options = {
         year: 'numeric',
@@ -21,7 +29,7 @@ function TodoCard(props) {
         shadow-sm md:hover:shadow-lg p-4 rounded-md md:hover:scale-105 cursor-pointer w-full bg-[#6B728E] text-white">
 
             <h1 className='font-Montserrat font-bold text-ellipsis md:max-w-[270px] whitespace-nowrap overflow-hidden'>{props.title}</h1>
-            <h1 className='font-Montserrat text-sm'>[{props.name}]</h1>
+            <h1 className='font-Montserrat text-sm'>{modifiedName}</h1>
             <h1 className='font-Montserrat mt-4 text-sm'>{props.description}</h1>
             <h1 className='font-Montserrat mt-4 text-xs font-bold'>{readableDate.toLocaleTimeString([], timeOptions)} - 
             {readableDate.toLocaleDateString('en-MY', options)}</h1>
